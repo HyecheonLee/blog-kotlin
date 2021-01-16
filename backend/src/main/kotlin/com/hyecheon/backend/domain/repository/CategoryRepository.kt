904ae5405db69d911acdb9d3947fdbbf967b3624
hyecheon <rainbow880616@gmail.com>
2021-01-17 02:51:46 +0900

    Category Api 구현...

--- backend/src/main/kotlin/com/hyecheon/backend/domain/repository/CategoryRepository.kt
+++ backend/src/main/kotlin/com/hyecheon/backend/domain/repository/CategoryRepository.kt
@@ -2,10 +2,16 @@ package com.hyecheon.backend.domain.repository
 
 import com.hyecheon.backend.domain.entity.*
 import org.springframework.data.jpa.repository.*
+import org.springframework.transaction.annotation.*
+import java.util.*
 
 /**
  * @author hyecheon
  * @email rainbow880616@gmail.com
  */
 interface CategoryRepository : JpaRepository<Category, Long> {
+	fun findBySlug(slug: String): Optional<Category>
+
+	@Transactional
+	fun deleteBySlug(slug: String)
 }
--- backend/src/main/kotlin/com/hyecheon/backend/web/api/CategoryController.kt
+++ backend/src/main/kotlin/com/hyecheon/backend/web/api/CategoryController.kt
@@ -2,6 +2,9 @@ package com.hyecheon.backend.web.api
 
 import com.hyecheon.backend.domain.entity.*
 import com.hyecheon.backend.domain.repository.*
+import javassist.*
+import org.springframework.data.domain.*
+import org.springframework.data.web.*
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
 import javax.validation.*
@@ -16,5 +19,22 @@ class CategoryController(private val categoryRepository: CategoryRepository) {
 	@PostMapping
 	fun create(@Valid @RequestBody category: Category) = run {
 		ResponseEntity.ok(categoryRepository.save(category))
+	}
+
+	@GetMapping
+	fun findAll(@PageableDefault(page = 0, size = 10, sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable) = run {
+		val findAll = categoryRepository.findAll(pageable)
+		ResponseEntity.ok(findAll)
+	}
+
+	@GetMapping("/{slug}")
+	fun findAll(@PathVariable slug: String) = run {
+		ResponseEntity.ok(categoryRepository.findBySlug(slug.toLowerCase()).orElseThrow { throw NotFoundException("slug[$slug]가 존재 하지 않습니다.") })
+	}
+
+	@DeleteMapping("/{slug}")
+	fun delete(@PathVariable slug: String) = run {
+		categoryRepository.deleteBySlug(slug.toLowerCase())
+		ResponseEntity.noContent()
 	}
 }
